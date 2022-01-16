8640b573f1f51b89b26516fac7f4b0b5dde74e77
Jakub Molicki <molickikuba@gmail.com>
2022-01-16 21:38:45 +0100

    single task styled changed

--- src/components/SingleTask/SingleTask.tsx
+++ src/components/SingleTask/SingleTask.tsx
@@ -1,11 +1,18 @@
-import React from "react";
-import { CheckTask, LabelTask, MainContainer } from "./SingleTaskStyled";
-
+import {
+  CheckTask,
+  LabelTask,
+  MainContainer,
+  ButtonTrash,
+} from "./SingleTaskStyled";
+import { BsFillTrashFill } from "react-icons/bs";
 export default function SingleTask() {
   return (
     <MainContainer>
       <CheckTask id="task" type="checkbox"></CheckTask>
-      <LabelTask htmlFor="task">Halo halo</LabelTask>
+      <LabelTask htmlFor="task">halo halo</LabelTask>
+      <ButtonTrash>
+        <BsFillTrashFill size={24}></BsFillTrashFill>
+      </ButtonTrash>
     </MainContainer>
   );
 }
--- src/components/SingleTask/SingleTaskStyled.tsx
+++ src/components/SingleTask/SingleTaskStyled.tsx
@@ -7,12 +7,47 @@ export const MainContainer = styled.div`
   border-radius: 8px;
   background: var(--dark-strong);
   margin: 20px auto 0;
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ButtonTrash = styled.button`
+  border: none;
+  background: transparent;
+  position: relative;
+  color: var(--red-orange);
+
+  right: 10px;
+  :hover {
+    cursor: pointer;
+  }
+  transition: all 0.2s;
+  :active {
+    transform: scale(1.05);
+  }
 `;
 
 export const CheckTask = styled.input`
   width: 24px;
   height: 24px;
   border: 0;
+
+  :checked + label {
+    text-decoration: line-through;
+    opacity: 0.4;
+  }
 `;
 
-export const LabelTask = styled.label``;
+export const LabelTask = styled.label`
+  color: var(--white);
+  width: 90%;
+  text-align: left;
+  position: relative;
+  padding-bottom: 2px;
+  border-bottom: 1px solid var(--orange);
+  ::first-letter {
+    text-transform: capitalize;
+  }
+`;
